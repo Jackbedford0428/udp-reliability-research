853dbcf6249acaf3baf58074de301cae212ef593
rtx4090x2 <jackbedford0428@gmail.com>
2024-03-12 19:47:09 +0800

    update single radio profiling method & some file naming tools

--- handover_profiling/my_utils.py
+++ handover_profiling/my_utils.py
@@ -4,6 +4,7 @@ import glob
 import re
 import ast
 import warnings
+import time
 
 import csv
 import json
@@ -36,7 +37,11 @@ import seaborn as sns
 __all__ = [
     "data_loader",
     "data_aligner",
-    "data_consolidator"
+    "data_consolidator",
+    "figure_identity",
+    "figure_add_prefix_suffix",
+    "model_prefix",
+    "model_add_prefix"
 ]
 
 with open("db_path.txt", "r") as f:
@@ -202,3 +207,85 @@ def data_consolidator(df1, df2, ho_df1, ho_df2):
     df2, ho_df2, _ = data_aligner(df2, ho_df2)
     
     return df, df1, df2, ho_df1, ho_df2, empty_data
+
+
+def datetime_to_str(timestamp_datetime):
+    return dt.datetime.strftime(timestamp_datetime, "%Y-%m-%d %H:%M:%S.%f")
+
+
+def str_to_datetime(timestamp_str, format='pd'):
+    if format == 'pd':
+        return pd.to_datetime(timestamp_str)
+    elif format == 'dt':
+        return dt.datetime.strptime(timestamp_str, "%Y-%m-%d %H:%M:%S.%f")
+
+
+def epoch_to_datetime(timestamp_epoch, format='pd', utc=8):
+    if format == 'pd':
+        return pd.to_datetime(timestamp_epoch, unit='s') + pd.Timedelta(hours=utc)
+    elif format == 'dt':
+        return dt.datetime.utcfromtimestamp(timestamp_epoch) + dt.timedelta(hours=utc)
+
+
+def datetime_to_epoch(timestamp_datetime, utc=8):
+    # Set the timezone
+    timezone = dt.timezone(dt.timedelta(hours=utc))
+    timestamp_datetime = timestamp_datetime.replace(tzinfo=timezone)
+    # Convert the datetime object to Unix timestamp
+    return timestamp_datetime.timestamp()
+
+
+def generate_hex_string(seed, length=16):
+    # 設定亂數種子
+    random.seed(seed)
+    # 生成隨機數
+    random_number = random.getrandbits(length * 4)  # 16進制的位數需要4位二進制數表示
+    # 轉換為16進位制字串
+    hex_string = hex(random_number)[2:]  # [2:]是因為hex()函數生成的字符串開頭是'0x'，需要去掉
+    return hex_string.zfill(length)  # 確保字串長度為length，不足的話在前面補0
+
+
+def figure_identity():
+    figure_identity.timestamp_str = getattr(figure_identity, 'timestamp_str', datetime_to_str(epoch_to_datetime(time.time())))
+    figure_identity.counter = getattr(figure_identity, 'counter', 0)
+    now = time.time()
+    date = "".join(figure_identity.timestamp_str[:10].split('-'))
+    hms_count = "".join(figure_identity.timestamp_str[11:19].split(':')) + str(figure_identity.counter).zfill(5)
+    hex_string = generate_hex_string(now)
+    figure_id = "_".join([date, hms_count, hex_string])
+    figure_identity.counter += 1
+    return date, hms_count, hex_string, figure_id
+
+
+def figure_add_prefix_suffix(image_path, image_id=None, suffix='.png'):
+    dirpath = os.path.dirname(image_path)
+    image_label = os.path.basename(image_path)
+    if image_label.endswith(suffix):
+        image_label = image_label.replace(suffix, '')
+    if image_id is None:
+        _, _, _, image_id = figure_identity()
+    image_path = "_".join([image_id[:8], image_label, image_id[9:]]) + suffix
+    image_path = os.path.join(dirpath, image_path)
+    return image_path
+
+
+def model_prefix():
+    model_prefix.timestamp_str = getattr(model_prefix, 'timestamp_str', datetime_to_str(epoch_to_datetime(time.time())))
+    model_prefix.counter = getattr(model_prefix, 'counter', 0)
+    date = "".join(model_prefix.timestamp_str[:10].split('-'))
+    hour_count = model_prefix.timestamp_str[11:13] + str(model_prefix.counter).zfill(3)
+    now = time.time()
+    hex_string = generate_hex_string(now, 5)
+    model_id = date + '_' + hour_count + hex_string
+    model_prefix.counter += 1
+    return model_id
+
+
+def model_add_prefix(model_path, model_id=None):
+    dirpath = os.path.dirname(model_path)
+    model_label = os.path.basename(model_path)
+    if model_id is None:
+        model_id = model_prefix()
+    model_path = "_".join([model_id, model_label])
+    model_path = os.path.join(dirpath, model_path)
+    return model_path
